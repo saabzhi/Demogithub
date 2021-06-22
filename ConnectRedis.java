2ee2362671fd685de7d277040818a8bb5833de3e
saabzhi <2233622373@qq.com>
2021-06-22 23:40:32 +0800

    Create JedisAPI java file
    Create connection Jedis API java

---
+++ ConnectRedis.java
@@ -0,0 +1,8 @@
+import Jedis.client.redis.Jedis;
+pluble class HelloDemo{
+    public static void main(String[] args){
+        System.out.println("Hello Demo java");   
+        System.out.println("Hello Demo java"); 
+        Jedis jedis = new Jedis("192.168.99.140",7379);
+    }
+}
