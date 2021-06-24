337c1a7c9fd567084d0271da775c0290ae8838a1
saabzhi <2233622373@qq.com>
2021-06-24 22:31:13 +0800

    Update redisconnection v0.2
    update v0.2

--- ConnectRedis.java
+++ ConnectRedis.java
@@ -4,5 +4,7 @@ pluble class HelloDemo{
         System.out.println("Hello Demo java");   
         System.out.println("Hello Demo java"); 
         Jedis jedis = new Jedis("192.168.99.140",7379);
+        jedis.set("key1","values1");
+        System.out.println(jedis.get(key1));
     }
 }
